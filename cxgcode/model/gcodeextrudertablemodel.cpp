c230b511b9f5fc00d7b60f2ce39f39e4a37d7a49
weizhixiao <weizhixiao@creality.com>
2024-01-24 18:36:32 +0800

    featre:[MaterialList]G-code->耗材消耗
    Change-Id: I67e28535ad79fe252074e657b7d621cc20568826

---
+++ cxgcode/model/gcodeextrudertablemodel.cpp
@@ -0,0 +1,160 @@
+#include "gcodeextrudertablemodel.h"
+
+namespace cxgcode {
+
+ModelItem::ModelItem(QObject* parent)
+    : QObject(parent) 
+{
+    m_Model = new MaterialConsume();// { 6.23, 3.3 };
+    m_Flush = new MaterialConsume();;
+    m_FilamentTower = new MaterialConsume();
+
+    m_Model->weight = 5.23;
+    m_Model->length = 3.3;
+
+    m_Flush->weight = 5.23;
+    m_Flush->length = 3.3;
+
+    m_FilamentTower->weight = 5.23;
+    m_FilamentTower->length = 3.3;
+}
+
+ModelItem::~ModelItem() {}
+
+MaterialConsume* ModelItem::getDataByType(ModelItem::ConsumeType type)
+{
+    switch (type)
+    {
+    case cxgcode::ModelItem::CT_Color:
+        return modelMaterial();
+        break;
+    case cxgcode::ModelItem::CT_Model:
+        return modelMaterial();
+        break;
+    case cxgcode::ModelItem::CT_Flush:
+        return flushMaterial();
+        break;
+    case cxgcode::ModelItem::CT_FilamentTower:
+        return filamentTower();
+        break;
+    case cxgcode::ModelItem::CT_Total:
+        return modelMaterial();
+        break;
+    default:
+        break;
+    }
+
+    return nullptr;
+}
+
+MaterialConsume* ModelItem::modelMaterial() const
+{
+    return m_Model;
+}
+
+MaterialConsume* ModelItem::flushMaterial() const
+{
+    return m_Flush;
+}
+
+MaterialConsume* ModelItem::filamentTower() const
+{
+    return m_FilamentTower;
+}
+
+GcodeExtruderTableModel::GcodeExtruderTableModel(QObject* parent)
+: QAbstractTableModel(parent)
+{
+    m_Materials.append(new ModelItem(this));
+    m_Materials.append(new ModelItem(this));
+    m_Materials.append(new ModelItem(this));
+    m_Materials.append(new ModelItem(this));
+    m_Materials.append(new ModelItem(this));
+}
+
+void GcodeExtruderTableModel::setDataList(const QList<cxgcode::GcodeExtruderData>& data_list) {
+    beginResetModel();
+    m_ColorsData = data_list;
+    endResetModel();
+}
+
+int GcodeExtruderTableModel::rowCount(const QModelIndex& parent) const 
+{
+  return m_Materials.count();
+}
+
+int GcodeExtruderTableModel::columnCount(const QModelIndex & parent) const
+{
+    return 3;
+}
+
+QVariant GcodeExtruderTableModel::data(const QModelIndex& index, int role) const 
+{
+  QVariant result{ QVariant::Type::Invalid };
+
+  if (index.row() < 0 || index.row() >= rowCount() || rowCount() < 2) {
+    return result;
+  }
+
+  ModelItem* itemData = m_Materials.at(index.row());
+
+  switch (role) 
+  {
+      case ModelItem::CT_Color:
+      {
+          MaterialConsume* mc = itemData->getDataByType((ModelItem::ConsumeType)index.column());
+          return QVariant::fromValue(mc);
+      }
+      case ModelItem::CT_Model:
+      {
+          MaterialConsume* mc = itemData->getDataByType((ModelItem::ConsumeType)index.column());
+          return QVariant::fromValue(mc);
+      }
+      case ModelItem::CT_Flush:
+      {
+          MaterialConsume* mc = itemData->getDataByType((ModelItem::ConsumeType)index.column());
+          return QVariant::fromValue(mc);
+      }
+      case ModelItem::CT_FilamentTower:
+      {
+          MaterialConsume* mc = itemData->getDataByType((ModelItem::ConsumeType)index.column());
+          return QVariant::fromValue(mc);
+      }
+      case ModelItem::CT_Total:
+      {
+          MaterialConsume* mc = itemData->getDataByType((ModelItem::ConsumeType)index.column());
+          return QVariant::fromValue(mc);
+      }
+      
+      default:
+          break;
+  }
+
+  return QVariant();
+}
+
+QHash<int, QByteArray> GcodeExtruderTableModel::roleNames() const {
+    QHash<int, QByteArray> hash =  { {ModelItem::CT_Model, "ctModel"},
+        {ModelItem::CT_Color, "ctColor"},
+        {ModelItem::CT_Flush, "ctFlush"},
+        {ModelItem::CT_FilamentTower, "ctFilamentTower"},
+        {ModelItem::CT_Total, "ctTotal"}, };
+
+    return hash;
+}
+
+QVariant GcodeExtruderTableModel::headerData(int section, Qt::Orientation orientation, int role) const 
+{
+    if (role != Qt::DisplayRole)
+        return QVariant();
+
+    if (orientation == Qt::Horizontal) {
+        // 返回水平表头
+        return QString("Column %1").arg(section);
+    }
+    else {
+        // 返回垂直表头
+        return QString("Row %1").arg(section);
+    }
+}
+}; // namespace cxgcode
---
+++ cxgcode/model/gcodeextrudertablemodel.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#ifndef CXGCODE_MODEL_GCODEEXTRUDERTABLEMODEL_H_
+#define CXGCODE_MODEL_GCODEEXTRUDERTABLEMODEL_H_
+
+#include <QtCore/QList>
+#include <QtGui/QColor>
+
+#include "cxgcode/interface.h"
+#include "cxgcode/model/gcodeextruderlistmodel.h"
+
+namespace cxgcode {
+
+class MaterialConsume : public QObject //重量，长度
+{
+    Q_OBJECT
+public:
+    //Q_PROPERTY(double length READ getLength)
+    //Q_PROPERTY(double weight READ getLength)
+    
+    //double getLength() { return length;  }
+    //double getWeight() { return weight;  }
+    MaterialConsume(QObject* parent = nullptr):QObject(parent){ }
+    ~MaterialConsume() {}
+    MaterialConsume(const MaterialConsume& modelItem) {};
+    Q_INVOKABLE double getLength() { return length; };
+    Q_INVOKABLE double getWeight() { return weight; };
+
+    double length;
+    double weight;
+};
+Q_DECLARE_METATYPE(MaterialConsume)
+Q_DECLARE_METATYPE(MaterialConsume*)
+
+class ModelItem : public QObject 
+{
+    Q_OBJECT
+    public:
+    enum ConsumeType //消耗的类型
+    {
+        CT_Color,
+        CT_Model,
+        CT_Flush,
+        CT_FilamentTower,
+        CT_Total
+    };
+
+   ModelItem(QObject* parent = nullptr);
+   ~ModelItem();
+   MaterialConsume* getDataByType(ModelItem::ConsumeType type);
+
+private:
+    MaterialConsume* modelMaterial() const;
+    MaterialConsume* flushMaterial() const;
+    MaterialConsume* filamentTower() const;
+
+    MaterialConsume* m_Model = nullptr;
+    MaterialConsume* m_Flush = nullptr;
+    MaterialConsume* m_FilamentTower = nullptr;
+};
+
+class CXGCODE_API GcodeExtruderTableModel : public QAbstractTableModel 
+{
+  Q_OBJECT
+public:
+  explicit GcodeExtruderTableModel(QObject* parent = nullptr);
+  virtual ~GcodeExtruderTableModel() = default;
+  void GcodeExtruderTableModel::setDataList(const QList<GcodeExtruderData>& data_list);
+protected:
+  int rowCount(const QModelIndex& parent = QModelIndex{}) const override;
+  int columnCount(const QModelIndex & = QModelIndex()) const override;
+  QVariant data(const QModelIndex& index, int role = Qt::ItemDataRole::DisplayRole) const override;
+  QHash<int, QByteArray> roleNames() const override;
+  QVariant GcodeExtruderTableModel::headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+private:
+    QList<ModelItem* > m_Materials;
+    QList<QColor> m_Colors;
+    QList<cxgcode::GcodeExtruderData> m_ColorsData;
+};
+}  // namespace cxgcode
+
+#endif  // CXGCODE_MODEL_GCODEEXTRUDERTABLEMODEL_H_
